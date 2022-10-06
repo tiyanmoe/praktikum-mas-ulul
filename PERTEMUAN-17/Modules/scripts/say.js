4f4e74dd25be95265f2d2852918f3e010410a93a
tiyanmoe <titinnsriyani@gmail.com>
2022-10-06 11:46:39 +0700

    lanjutan

---
+++ PERTEMUAN-17/Modules/scripts/say.js
@@ -0,0 +1,10 @@
+// untuk bisa export harus dikasih keterangan export pada awalan variabel, class, maupun function
+export function sayHello(name) {
+    console.info(`Hello ${name}`);
+}
+
+export function sayGoodBye(name) {
+    console.info(`Good Bye ${name}`);
+}
+
+export const name = "Titin"
---
+++ PERTEMUAN-17/Modules/scripts/variable2.js
@@ -0,0 +1,1 @@
+export var name = "Do Kyungsoo";
